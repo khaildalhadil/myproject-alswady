432b273f16c3fa28b67bef5112bdd35591c32623
Khaild <khaild.alhadi2020@gmail.com>
2024-05-05 21:08:35 +0400

    add

--- javascript/video.js
+++ javascript/video.js
@@ -6,6 +6,19 @@ const progressBar = document.querySelector('.progress-bar');
 const skipBackward = document.querySelector('.skip-backward i');
 const skipForward = document.querySelector('.skip-forward i');
 const volumeBtn = document.querySelector('.vlume-btn i');
+const volumeSlider = document.querySelector('.left input');
+const playButton = document.querySelector('.play-btn');
+const xBtn = document.querySelector('.x-btn');
+
+playButton.addEventListener('click', ()=> {
+    videoContainer.classList.add('show-video');
+})
+
+xBtn.addEventListener('click', ()=> {
+    videoContainer.classList.remove('show-video');
+    mainVedeo.pause();
+})
+
 
 playPauseBtn.addEventListener('click', ()=> {
     if(mainVedeo.paused) {
@@ -13,6 +26,15 @@ playPauseBtn.addEventListener('click', ()=> {
         
     } else {
         mainVedeo.pause();
+    }
+})
+
+volumeSlider.addEventListener('input', (e)=> {
+    mainVedeo.volume = e.target.value * 1
+    if (mainVedeo.volume === 0) {
+        volumeBtn.classList.replace('fa-volume-high', 'fa-volume-xmark')
+    } else {
+        volumeBtn.classList.replace('fa-volume-xmark', 'fa-volume-high')
     }
 })
 
@@ -47,4 +69,6 @@ volumeBtn.addEventListener('click', ()=> {
         mainVedeo.volume = 0.0
         volumeBtn.classList.replace('fa-volume-high', 'fa-volume-xmark')
     }
+
+    volumeSlider.value = mainVedeo.volume
 })
